34ea5122690ded121107399a0f88dba917498ca8
swxft <edenweb@outlook.com>
2022-10-03 13:40:10 -0400

    solution including number input + es lint added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const phoneFormatter = (digits) => {
-    const formattedArr = Array.from(digits)
+    const stringInput = digits.toString()
+    const formattedArr = Array.from(stringInput)
     formattedArr.splice(0, 0, '(')
     formattedArr.splice(4, 0, ')')
     formattedArr.splice(-4, 0, '-')
     return formattedArr.join('')
 }
-console.log(phoneFormatter('4448916577'))
+// console.log(phoneFormatter('4448916567'))
+// console.log(phoneFormatter(4448916577))
+console.log(phoneFormatter(4155448375))
